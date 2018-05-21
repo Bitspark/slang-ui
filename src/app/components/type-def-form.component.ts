fb34bb95da2b9b80b07fcf81517b20f19085bcd4
Taleh Didover <taleh.didover@bitspark.de>
2018-05-21 19:02:00 +0200

    Enable adding, renaming and removing map ports

--- src/app/components/type-def-form.component.ts
+++ src/app/components/type-def-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {OperatorDef, Port, PortType} from '../classes/operator';
+import {OperatorDef, PortType} from '../classes/operator';
 
 @Component({
   selector: 'app-type-def-form',
@@ -8,27 +8,63 @@ import {OperatorDef, Port, PortType} from '../classes/operator';
 })
 export class TypeDefFormComponent implements OnInit {
   @Input()
-  public port: any = {
-    type: 'primitive'
-  };
+  public port: any = TypeDefFormComponent.newPrimitiveTypeDef();
   public portTypes = Object.keys(PortType).filter(t => typeof PortType[t] === 'number');
+  public newMapPortName: string;
 
   constructor() {
+  }
+
+  public static newPrimitiveTypeDef(): any {
+    return {
+      type: 'primitive'
+    };
   }
 
   ngOnInit() {
   }
 
+
   public setType(portType: string) {
-    this.port[portType] = {
-      type: 'primitive'
-    };
+    if (portType === 'map') {
+      this.port[portType] = new Map<string, any>();
+    } else {
+      this.port[portType] = TypeDefFormComponent.newPrimitiveTypeDef();
+    }
     this.port.type = portType;
+  }
+
+  public getMapPortNames(): Array<string> {
+    if (this.isMap()) {
+      return Array.from(this.port.map.keys());
+    }
+    return [];
+  }
+
+  public addMapPort(portName: string) {
+    if (!portName || this.port.map.has(portName)) {
+      return;
+    }
+
+    this.port.map.set(portName, TypeDefFormComponent.newPrimitiveTypeDef());
+    this.resetNewMapPortName();
+  }
+
+  public renameMapPortName(oldPortName, newPortName: string) {
+    this.port.map.set(newPortName, this.port.map.get(oldPortName));
+    this.removeMapPort(oldPortName);
+  }
+
+  public removeMapPort(portName: string) {
+    this.port.map.delete(portName);
+  }
+
+  public resetNewMapPortName() {
+    this.newMapPortName = '';
   }
 
   public isMap(): boolean {
     return OperatorDef.isMap(this.port);
-
   }
 
   public isStream(): boolean {
@@ -38,4 +74,5 @@ export class TypeDefFormComponent implements OnInit {
   public isGeneric(): boolean {
     return OperatorDef.isGeneric(this.port);
   }
+
 }
